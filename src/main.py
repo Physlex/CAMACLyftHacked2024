5744d488511dde658de0ea4a36d9e20d14302b3b
unknown <al.willet02@gmail.com>
2024-01-06 11:56:12 -0700

    download api avaliable

--- src/main.py
+++ src/main.py
@@ -1,4 +1,8 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi.responses import JSONResponse
+from fastapi.staticfiles import StaticFiles
+
 import uvicorn
 
 from models import User
@@ -7,11 +11,21 @@ app = FastAPI()
 
 ### API
 
-# @app.mount()
-# @app.mount()
-# @app.mount()
+@app.mount("/templates", StaticFiles(directory="/templates"), name="/templates")
+@app.mount("/static/css", StaticFiles(directory="/static/css"), name="/static/css")
+@app.mount("/static/data", StaticFiles(directory="/static/data", name="/static/data"))
+@app.mount("/static/js", StaticFiles(directory="/static/js"), name="/static/js")
 
-# def render():
+@app.get("/", response_class=HTMLResponse)
+async def index():
+    pass
+
+@app.get("/download")
+async def download():
+    """
+        TODO: Takes a userID and returns it's associated streamed data
+    """
+    return JSONResponse([])
 
 
 ## MAIN
