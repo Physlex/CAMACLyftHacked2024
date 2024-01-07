5898d9ab105f5303d655d1a3923f01b683beb82a
unknown <al.willet02@gmail.com>
2024-01-06 23:23:31 -0700

    sockets

--- src/socketManager.py
+++ src/socketManager.py
@@ -1,27 +1,28 @@
 from fastapi import WebSocket
 
-class SocketManager():
-    def __init__(self, websocket: WebSocket) -> None:
-        self.websocket = websocket
+class SocketMan():
+    def __init__(self) -> None:
+        self.socket = WebSocket()
         pass
 
-    async def connect(self) -> None:
+    async def connect(self, websocket: WebSocket) -> None:
         """
-            Connect to the websocket
+            Connect to the websocket and add it to all web
         """
-        await self.websocket.accept()
+        await websocket.accept()
+        self.socket = websocket
         pass
 
     async def disconnect(self) -> None:
         """
-            Disconnect from the websocket
+            Disconnect from all websockets
         """
-        await self.websocket.close()
+        await self.socket.close()
         pass
 
     async def send_acceleration(self, data) -> None:
         """
             TODO: Get acceleration data from arduino
         """
-        self.websocket.send_json("""SOME DATA""")
+        self.socket.send_json("""SOME DATA""")
         pass
