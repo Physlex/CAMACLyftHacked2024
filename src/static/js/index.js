c688f7ba4dbafb07380587b0b332e334c40e4bfa
unknown <al.willet02@gmail.com>
2024-01-06 21:49:49 -0700

    frontend fixed... again

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -1,7 +1,7 @@
 
 const url = "http://127.0.0.1:8000";  // Replace with your correct URL
 
-button.addEventListener("click", async () => {
+document.querySelector("#connection-btn").addEventListener("click", async (event) => {
     let response = await fetch(url + "/download");
     if (response.ok) {
         let data = await response.json();
@@ -13,12 +13,11 @@ button.addEventListener("click", async () => {
 
 document.querySelector("#username-form").addEventListener("submit", async (event) => {
     let userName = document.querySelector('#username-form').value;
-    fetch(url + "/authenticate", {
+    let response = await fetch(url + "/authenticate", {
         method: 'POST',
         headers: { 'Content-Type': 'application/json'},
         body: JSON.stringify({ variable: userName }),
     });
-
     if (response.ok) {
         let json_response = await response.json();
         console.log(json_response);
